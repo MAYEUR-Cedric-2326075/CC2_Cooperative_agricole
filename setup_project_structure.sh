82d0ffea1f159a1cfb1fa731130f4a97a783dc8c
DNicolas7 <152083265+DNicolas7@users.noreply.github.com>
2025-04-02 00:01:50 +0200

    Arboréssance mise à jour

---
+++ setup_project_structure.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+mkdir -p control && \
+mkdir -p control && \
+touch control/Controllers.php && \
+mkdir -p control && \
+touch control/Presenter.php && \
+mkdir -p data && \
+mkdir -p data && \
+touch data/ApiBasket.php && \
+mkdir -p data && \
+touch data/ApiOrder.php && \
+mkdir -p data && \
+touch data/ApiProduct.php && \
+mkdir -p data && \
+touch data/ApiUser.php && \
+mkdir -p domain && \
+mkdir -p domain && \
+touch domain/Order.php && \
+mkdir -p domain && \
+touch domain/Basket.php && \
+mkdir -p domain && \
+touch domain/Product.php && \
+mkdir -p domain && \
+touch domain/User.php && \
+mkdir -p service && \
+mkdir -p service && \
+touch service/OrderService.php && \
+mkdir -p service && \
+touch service/BasketService.php && \
+mkdir -p service && \
+touch service/UserService.php && \
+mkdir -p service && \
+touch service/ProductService.php && \
+mkdir -p service/interfaces && \
+touch service/interfaces/OrderAccessInterface.php && \
+mkdir -p service/interfaces && \
+touch service/interfaces/BasketAccessInterface.php && \
+mkdir -p service/interfaces && \
+touch service/interfaces/ProductAccessInterface.php && \
+mkdir -p service/interfaces && \
+touch service/interfaces/UserAccessInterface.php && \
+mkdir -p gui && \
+mkdir -p gui && \
+touch gui/ViewBasketList.php && \
+mkdir -p gui && \
+touch gui/ViewBasketDetails.php && \
+mkdir -p gui && \
+touch gui/ViewOrders.php && \
+mkdir -p gui && \
+touch gui/ViewOrderConfirmation.php && \
+mkdir -p gui && \
+touch gui/ViewLogin.php && \
+mkdir -p gui && \
+touch gui/ViewRegisterCustomer.php && \
+mkdir -p gui && \
+touch gui/ViewManageBaskets.php && \
+mkdir -p gui && \
+touch gui/ViewEditBasket.php && \
+mkdir -p gui && \
+touch gui/ViewSubscribers.php && \
+mkdir -p gui && \
+touch gui/ViewRegistrationManager.php && \
+mkdir -p gui && \
+touch gui/ViewLoggedCustomer.php && \
+mkdir -p gui && \
+touch gui/ViewLoggedManager.php && \
+mkdir -p gui && \
+touch gui/ViewUnlogged.php && \
+mkdir -p gui && \
+touch gui/Layout.php && \
+mkdir -p gui && \
+touch gui/ViewError.php && \
+mkdir -p gui && \
+touch gui/View.php && \
+mkdir -p gui/js && \
+mkdir -p gui/js && \
+touch gui/js/viewBasketForm.js && \
+touch index.php && \
+touch README.md
